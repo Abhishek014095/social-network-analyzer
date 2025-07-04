39acb86a5bd48724d3cc284726b4324ff02e1221
Abhishek014095 <sharma.abhi14095@gmail.com>
2025-07-04 18:18:27 +0530

    routes added

--- social-network-analyzer/web.py
+++ social-network-analyzer/web.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import json
 
+from werkzeug.utils import redirect
 
 with open("config.json") as f:
     params = json.load(f)["params"]
@@ -26,6 +27,35 @@ def login():
         # print(jsonify({"message": "Login successful"})), 200
     else:
         return jsonify({"message": "Invalid credentials"}), 401
+
+
+
+@app.route("/logout")
+def logout():
+    return  redirect("http://127.0.0.1:5000/")
+    # return render_template("index.html")
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route('/users')
+def users():
+    return render_template("users.html")
+
+@app.route('/queries')
+def queries():
+    return  render_template("queries.html")
+
+
+@app.route('/user-ratings')
+def user_ratings():
+    return  render_template("reviews.html")
+
+@app.route("/announcements")
+def announcements():
+    return  render_template("")
 
 if __name__ == '__main__':
     app.run(debug=True)
